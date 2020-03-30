899217c8519164740a59d17b6c04f31f5ce3e6c8
Jean Mayorga <jpmayorga@outlook.com>
2020-03-30 00:34:10 -0500

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './src/less/theme.less'), 'utf8')
+)
+
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: themeVariables,
+  }),
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import React from 'react';
+
+export function Home() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button>Hola</Button>
+      </header>
+    </div>
+  );
+}
